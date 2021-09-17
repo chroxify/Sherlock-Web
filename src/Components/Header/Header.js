3296674c6cac8d545f179b579775bf25e6aefcf1
Christo Todorov <chroxify@gmail.com>
2021-09-17 20:06:49 +0200

    finish front end

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,22 @@
 // React
 import React from 'react';
 import Image from '../../Images/Github-Mark.png'
+
 // CSS
 import './Header.css';
 
 const Header = () => {
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
+
     return (
         <div className="header">
             <p className="text">Made with ❤️ by Chroxify</p>
-            <a href="https://github.com/Chroxify/Sherlock-Web">
-                <img src={Image} alt="" className="gt-logo" />
-            </a>
+
+            <img src={Image} alt="" className="gt-logo" onClick={() => openInNewTab("https://github.com/Chroxify/Sherlock-Web")} />
+
         </div>
     );
 }
--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -10,18 +10,18 @@ const MainMenu = () => {
         <div className="card">
             <h1>Results for Chroxify:</h1>
             <section className="basic-grid">
-                <div className="item"> <Icon icon="dashicons:yes" className="icon-yes" /> 1ajishahsahzshasa</div>
-                <div className="item">2</div>
-                <div className="item">3</div>
-                <div className="item">4</div>
-                <div className="item">5</div>
-                <div className="item">6</div>
-                <div className="item">7</div>
-                <div className="item">8</div>
-                <div className="item">9</div>
-                <div className="item">10</div>
-                <div className="item">11</div>
-                <div className="item">12</div>
+                <a href="/#" target="_blank" className="item"> <Icon icon="dashicons:yes" className="icon-yes" /> 1</a>
+                <a href="/#" target="_blank" className="item">2</a>
+                <a href="/#" target="_blank" className="item">3</a>
+                <a href="/#" target="_blank" className="item">4</a>
+                <a href="/#" target="_blank" className="item">5</a>
+                <a href="/#" target="_blank" className="item">6</a>
+                <a href="/#" target="_blank" className="item">7</a>
+                <a href="/#" target="_blank" className="item">8</a>
+                <a href="/#" target="_blank" className="item">9</a>
+                <a href="/#" target="_blank" className="item">10</a>
+                <a href="/#" target="_blank" className="item">11</a>
+                <a href="/#" target="_blank" className="item">12</a>
             </section>
         </div>
     );
